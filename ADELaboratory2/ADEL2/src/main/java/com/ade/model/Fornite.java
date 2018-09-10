d30ce61a60bbd47893fcc4a2e37ef58f540e5e08
Jamf99 <jamf_99@hotmail.com>
2018-09-10 12:51:43 -0500

    Fornite class was modified
    Created methods that allow add players, and classify the plays taking
    into account your platform

--- ADELaboratory2/ADEL2/src/main/java/com/ade/model/Fornite.java
+++ ADELaboratory2/ADEL2/src/main/java/com/ade/model/Fornite.java
@@ -3,6 +3,8 @@ package com.ade.model;
 public class Fornite {
 	
 	private Plataform[] plataforms;
+	private Player firstPlayer;
+	private int playersSize;
 	
 	public Fornite() {
 		plataforms = new Plataform[4];
@@ -12,8 +14,49 @@ public class Fornite {
 		plataforms[3] = new Plataform("Smartphone");
 	}
 	
+	public Fornite(int x) {
+		firstPlayer = null;
+		playersSize = 0;
+	}
+	
+	public Player getFirstPlayer() {
+		return firstPlayer;
+	}
+	
+	public int getPlayersSize() {
+		return playersSize;
+	}
+	
 	public Plataform[] getPlataforms() {
 		return plataforms;
+	}
+	
+	public boolean isEmpty() {
+		if(playersSize == 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void matchPlayersWithoutPlataformMode() {
+		
+	}
+	
+	public void add(Player player) {
+		Player p = new Player(player.getName(), player.getDeaths(), player.getKills(), player.getVictories(), player.getGamesPlayed(),
+				player.getAveragePing(), player.getPlataform());
+		if (isEmpty()) {
+			firstPlayer = p;
+		} else {
+			Player aux = firstPlayer;
+			while (aux.getNext() != null) {
+				aux = aux.getNext();
+			}
+
+			aux.setNext(p);
+		}
+		playersSize++;
 	}
 
 }
