f4827b59252ac67efa1dd714abf19b32b3032cec
Jamf99 <jamf_99@hotmail.com>
2018-09-12 09:04:43 -0500

    Generated test to the class Fornite

---
+++ ADELaboratory2/ADEL2/src/test/java/com/ade/tests/ForniteTest.java
@@ -0,0 +1,14 @@
+package com.ade.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ForniteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
