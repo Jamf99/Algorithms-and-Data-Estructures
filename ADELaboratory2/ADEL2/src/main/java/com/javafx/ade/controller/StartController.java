40a928c150c65e1e93d27aed81022f2fabe4320c
kevinzarama <zaramaluna1999@hotmail.com>
2018-09-15 21:17:06 -0500

    fixed

--- ADELaboratory2/ADEL2/src/main/java/com/javafx/ade/controller/StartController.java
+++ ADELaboratory2/ADEL2/src/main/java/com/javafx/ade/controller/StartController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -97,18 +98,39 @@ public class StartController implements Initializable{
 		return platforms.getSelectionModel().getSelectedIndex();
 	}
 	
+	public boolean havePlatform() {
+		if(platforms.getSelectionModel().getSelectedIndex() == 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 	@FXML
     private Label txtPlatform;
 
 	@FXML
+    private CheckBox isPlatformMode;
+	
+	@FXML
+	void isPlatformMode(ActionEvent event) {
+		if(isPlatformMode.isSelected()) {
+			platforms.setDisable(false);
+			fornite = new Fornite(true);
+		}
+		if(!isPlatformMode.isSelected()) {
+			platforms.setDisable(true);
+			fornite = new Fornite(false);
+		}
+	}
+	
+	@FXML
 	void addPlayer(ActionEvent event) {
 		try {
-			Player p = new Player(getName(), getKills(), getVictories(), getGamesPlayed(), getAveragePing(), getPlatform());
-			boolean havePlatform = false;
-			if(getPlatform() == 0) {
-				fornite = new Fornite(1);
-				havePlatform = true;
-				fornite.addPlayer(p, havePlatform);
+			Player p = null;
+			if(!isPlatformMode.isSelected()) {
+				p = new Player(getName(), getKills(), getVictories(), getGamesPlayed(), getAveragePing(), -1);
+				fornite.addPlayer(p, false);
 				noobLow.setText(fornite.getNoobLow().size()+"");
 				noobMid.setText(fornite.getNoobMid().size()+"");
 				noobHigh.setText(fornite.getNoobHigh().size()+"");
@@ -118,17 +140,11 @@ public class StartController implements Initializable{
 				proLow.setText(fornite.getProLow().size()+"");
 				proMid.setText(fornite.getProMid().size()+"");
 				proHigh.setText(fornite.getProHigh().size()+"");
-				name.setText("");
-				kills.setText("");
-				averagePing.setText("");
-				victories.setText("");
-				gamesPlayed.setText("");
-				txtPlatform.setText(platforms.getSelectionModel().getSelectedItem());
+				txtPlatform.setText("Whithout Platform");	
 			}else {
-				fornite = new Fornite();
-				String platform = platforms.getSelectionModel().getSelectedItem();
-				int posPlatform = platforms.getSelectionModel().getSelectedIndex()-1;
-				fornite.addPlayer(p, havePlatform);
+				p = new Player(getName(), getKills(), getVictories(), getGamesPlayed(), getAveragePing(), getPlatform());
+				int posPlatform = platforms.getSelectionModel().getSelectedIndex();
+				fornite.addPlayer(p, true);
 				noobLow.setText(fornite.getPlataforms()[posPlatform].getNoobLow().size()+"");
 				noobMid.setText(fornite.getPlataforms()[posPlatform].getNoobMid().size()+"");
 				noobHigh.setText(fornite.getPlataforms()[posPlatform].getNoobHigh().size()+"");
@@ -138,14 +154,13 @@ public class StartController implements Initializable{
 				proLow.setText(fornite.getPlataforms()[posPlatform].getProLow().size()+"");
 				proMid.setText(fornite.getPlataforms()[posPlatform].getProMid().size()+"");
 				proHigh.setText(fornite.getPlataforms()[posPlatform].getProHigh().size()+"");
-				name.setText("");
-				kills.setText("");
-				averagePing.setText("");
-				victories.setText("");
-				gamesPlayed.setText("");
-				txtPlatform.setText(platform);
+				txtPlatform.setText(platforms.getSelectionModel().getSelectedItem());	
 			}
-			
+			name.setText("");
+			kills.setText("");
+			averagePing.setText("");
+			victories.setText("");
+			gamesPlayed.setText("");
 		}catch(ArithmeticException s) {
 			Alert message = new Alert(Alert.AlertType.ERROR);
 			message.setTitle("Error");
@@ -163,7 +178,8 @@ public class StartController implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		ObservableList<String> states = FXCollections.observableArrayList("Wihout platform", "PlayStation", "Xbox", "PC", "Smarthphone", "Nintendo Switch");
+		fornite = new Fornite(false);
+		ObservableList<String> states = FXCollections.observableArrayList("PlayStation", "Xbox", "PC", "Smarthphone", "Nintendo Switch");
 		platforms.setItems(states);
 		platforms.getSelectionModel().select(0);
 	}
